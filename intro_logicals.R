56177921f63b1a616c748845ea70a5740ae7a428
sofiasarak <sofiasarak30@gmail.com>
2025-08-08 13:54:08 -0700

    logical practice

---
+++ intro_logicals.R
@@ -0,0 +1,30 @@
+rm(list=ls()) #deleting everything in enviro
+
+dogs <- c(1,10,4,5,18)
+cats <- c(0,12,10,5,2)
+
+dogs < cats
+
+5 == 5
+5 == 4
+"cat" == "dog"
+1 == TRUE
+0 == FALSE
+
+5 != 6
+cats != dogs
+
+#logical and's
+5 < 10 & "cat" < "dog"
+9 > 4 & 2 < 3
+
+bananas <- c(10,21,12,15,22)
+apples <- c(9,18,16,20,30)
+
+apples < bananas & bananas > 20
+
+#logical or's
+
+4<10 | 100<2
+
+5>6 | "dog" < "cat" | 10<50
